59b940dc5ee46893e84bb85afc7ec6e83c6c8aa4
Guilherme Biassio <guilherme.cruz.biassio@gmail.com>
2024-01-17 17:53:47 -0300

    add breeze

--- resources/views/components/instructors/form.blade.php
+++ resources/views/components/instructors/form.blade.php
@@ -15,70 +15,57 @@
 
     <div class="mb-3 input-group-lg">
         <label for="status" class="form-label">Data</label>
-        <input type="date" class="form-control" id="data" name="data_instrucao" 
-        @isset($dados) value="{{ $dados->data_instrucao}}"  @endisset
-        @if($errors->any())
-            value="{{ old('data_instrucao') }}"
-        @endif
-        >        
+        <input type="date" class="form-control" id="data" name="data_instrucao"
+            @isset($dados) value="{{ $dados->data_instrucao }}"  @endisset
+            @if ($errors->any()) value="{{ old('data_instrucao') }}" @endif>
     </div>
     <div class="mb-3 input-group-lg">
         <label for="status" class="form-label">Status</label>
-        <input type="text" class="form-control" id="status" name="status" 
-        @isset($dados) value="{{ $dados->status}}" @endisset
-        @if($errors->any())
-            value="{{ old('status') }}"
-        @endif
-        >        
+        <input type="text" class="form-control" id="status" name="status"
+            @isset($dados) value="{{ $dados->status }}" @endisset
+            @if ($errors->any()) value="{{ old('status') }}" @endif>
     </div>
     <div class="mb-3 input-group-lg">
         <label for="motorista" class="form-label">Motorista</label>
-        <input type="text" class="form-control" id="motorista" name="motorista" 
-        @isset($dados) value="{{ $dados->motorista}}" @endisset
-        @if($errors->any())
-            value="{{ old('motorista') }}"
-        @endif
-        >
+        <input type="text" class="form-control" id="motorista" name="motorista"
+            @isset($dados) value="{{ $dados->motorista }}" @endisset
+            @if ($errors->any()) value="{{ old('motorista') }}" @endif>
     </div>
     <div class="mb-3 input-group-lg">
         <label for="carro" class="form-label">Carro</label>
-        <input type="text" class="form-control" id="carro" name="carro" 
-        @isset($dados) value="{{ $dados->carro}}" @endisset
-        @if($errors->any())
-            value="{{ old('carro') }}"
-        @endif
-        >
+        <input type="text" class="form-control" id="carro" name="carro"
+            @isset($dados) value="{{ $dados->carro }}" @endisset
+            @if ($errors->any()) value="{{ old('carro') }}" @endif>
     </div>
     <div class="mb-3 input-group-lg">
         <label for="linha" class="form-label">Linha</label>
-        <input type="text" class="form-control" id="linha" name="linha" 
-        @isset($dados) value="{{ $dados->linha}}" @endisset
-        @if($errors->any())
-            value="{{ old('linha') }}"
-        @endif
-        >
+        <input type="text" class="form-control" id="linha" name="linha"
+            @isset($dados) value="{{ $dados->linha }}" @endisset
+            @if ($errors->any()) value="{{ old('linha') }}" @endif>
     </div>
     <div class="mb-3 input-group-lg">
         <label for="ini" class="form-label">Início Percurso</label>
-        <input type="text" class="form-control" id="ini" name="inicio_percurso" 
-        @isset($dados) value="{{ $dados->inicio_percurso}}" @endisset
-        @if($errors->any())
-            value="{{ old('inicio_percurso') }}"
-        @endif
-        >
+        <input type="text" class="form-control" id="ini" name="inicio_percurso"
+            @isset($dados) value="{{ $dados->inicio_percurso }}" @endisset
+            @if ($errors->any()) value="{{ old('inicio_percurso') }}" @endif>
     </div>
     <div class="mb-3 input-group-lg">
         <label for="fim" class="form-label">Final Percurso</label>
-        <input type="text" class="form-control" id="fim" name="final_percurso" 
-        @isset($dados) value="{{ $dados->final_percurso}}" @endisset
-        @if($errors->any())
-            value="{{ old('final_percurso') }}"
-        @endif
-        >
+        <input type="text" class="form-control" id="fim" name="final_percurso"
+            @isset($dados) value="{{ $dados->final_percurso }}" @endisset
+            @if ($errors->any()) value="{{ old('final_percurso') }}" @endif>
     </div>
     <div class="form-floating mb-2">
-        <textarea class="form-control" placeholder="Observações" id="obs" style="height: 200px" name="observacoes">@isset($dados) {{ $dados->observacoes}} @endisset @if($errors->any()){{ old('observacoes') }} @endif </textarea>
+        <textarea class="form-control" placeholder="Observações" id="obs" style="height: 200px" name="observacoes">
+@isset($dados)
+{{ $dados->observacoes }}
+@endisset @if ($errors->any())
+{{ old('observacoes') }}
+@endif
+</textarea>
         <label for="obs">Observações</label>
     </div>
-    <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+    <div class="d-grid gap-2">
+        <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+    </div>
 </form>
--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -10,33 +10,46 @@
 
 <body>
 
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
-            <h3 class="navbar-brend"><a class="link-offset-2 link-underline link-underline-opacity-0" href="/">Instrutores</a></h3>
-            <button class="btn btn-primary btn-lg" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop"
-                aria-controls="staticBackdrop">
-                <i class="bi bi-list"></i>
-            </button>
-        </div>
-    </nav>
+    @if (Auth::check())
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <h3 class="navbar-brend"><a class="link-offset-2 link-underline link-underline-opacity-0"
+                        href="/">Instrutores</a></h3>
+                <button class="btn btn-primary btn-lg" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+                    <i class="bi bi-list"></i>
+                </button>
+            </div>
+        </nav>
 
-    <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop"
-        aria-labelledby="staticBackdropLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="staticBackdropLabel">Menu</h5>
-            <button type="button" class="btn-close btn-lg" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop"
+            aria-labelledby="staticBackdropLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="staticBackdropLabel">Menu</h5>
+                <button type="button" class="btn-close btn-lg" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="nav flex-column">
+                    <li class="nav-item d-grid gap-2 mt-2">
+                        <a class="btn btn-primary" href="{{ route('instrutores.create') }}">Diário de Instrução</a>
+                    </li>
+                    <li class="nav-item d-grid gap-2 mt-2">
+                        <a class="btn btn-primary" href="#">Filtrar Período</a>
+                    </li>
+                </ul>
+            </div>
+
+            <hr>
+
+            <div class="w-100 d-flex flex-column align-items-center">
+                <h6>{{ Auth::user()->name }}</h6>
+                <form class="w-75 m-3 d-grid gap-2" action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <button class="btn btn-danger">Sair</button>
+                </form>
+            </div>
         </div>
-        <div class="offcanvas-body">
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                  <a class="nav-link active" href="{{ route('instrutores.create') }}">Diário de Instrução</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Filtrar Período</a>
-                </li>
-              </ul>
-        </div>
-    </div>
+    @endif
     <div class="container">
         @yield('content')
     </div>
